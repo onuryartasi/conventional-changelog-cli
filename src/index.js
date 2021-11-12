9c2d28b9caa7f63342d6d527e52bd2efe60da3ec
Onur Yartaşı <onuryartasi@live.com>
2021-11-12 16:06:03 +0300

    fix branch exists

--- src/index.js
+++ src/index.js
@@ -295,7 +295,7 @@ async function run(parameters) {
         console.log("end pull")
         console.log(pull)
         await git.tag(["-a","-f",gitTag,"-m",stringChangelog])
-        await git.push("origin",'--force','-u',`refs/heads/${gitTag}:refs/heads/${gitTag}`)
+        await git.push(["origin",'--force',`refs/heads/${gitTag}:refs/heads/${gitTag}`])
         await git.push("origin",'--tags','--force')
        // await git.pushTags()
 
