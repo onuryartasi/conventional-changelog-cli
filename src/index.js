b1854c448e96b6ff541ea672ff694e2c0a197be4
Onur Yartaşı <onuryartasi@live.com>
2021-11-12 09:35:43 +0300

    added push tags force

--- src/index.js
+++ src/index.js
@@ -286,7 +286,8 @@ async function run(parameters) {
         await git.add([versionFile,outputFile])
         await git.commit(gitMessage)
         await git.tag(["-a",gitTag,"-m",stringChangelog])
-        await git.push("origin",`refs/heads/${gitTag}:refs/heads/${gitTag}`,["--atomic","--tags"])
+        await git.push("origin",`refs/heads/${gitTag}:refs/heads/${gitTag}`)
+        await git.pushTags("origin",{"--force":"true"})
        // await git.pushTags()
         await octokit.rest.pulls.create({
           owner: repository.owner,
