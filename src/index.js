204f086915db2420d058fee2f355840620edbd2d
Onur Yartaşı <onuryartasi@live.com>
2021-11-12 11:21:41 +0300

    fix pushTags

--- src/index.js
+++ src/index.js
@@ -284,9 +284,9 @@ async function run(parameters) {
         await git.checkoutLocalBranch(gitTag);
         await git.add([versionFile,outputFile])
         await git.commit(gitMessage)
-        await git.tag(["-af",gitTag,"-m",stringChangelog])
+        await git.tag(["-a",gitTag,"-m",stringChangelog])
         await git.push("origin",`refs/heads/${gitTag}:refs/heads/${gitTag}`)
-        await git.pushTags("origin",{"--force":"true"})
+        await git.pushTags("origin")
        // await git.pushTags()
         await octokit.rest.pulls.create({
           owner: repository.owner,
